bacf83e37f9a025b0047e0d65b459ecb86f34852
mamugo1432 <mamugo1432@gmail.com>
2025-03-25 09:55:00 +0100

    Añadido ejercicio 1 realizado

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 /*Una variable numero cuyo valor sea -4.*/
+let numero=-4;
 /*Una variable palabra cuyo valor sea ”Hola Mundo”*/
 /*Una variable booleana cuyo nombre sea respuesta e 
 inicialmente con valor true*/
 /*Crea una constante con nombre pi cuyo valor sea 3.14*/
 /*Modifica el valor de la variable numero para que en lugar 
 de -4 sea 54 pero sin tocar la parte de la declaración de la variable.*/
-/*¿Qué pasa si modificas el valor de la variable antes de que esta sea declarada?
-el consola Muestra nombre se por de utiliza consola la
-variable la JAVASCRIPT instrucción los valores a pintar.
- console.log(XXXXXXXXX); de todas las variables creadas.  
- Donde Para XXXXX pintar será
-   por o Modifica el valor de pi por el valor 3.1415. ¿Qué ocurre?.*/
+/*¿Qué pasa si modificas el valor de la variable antes de que esta sea declarada?*/
 /*Declara 2 variables y asigna los valores 5 y 2 respectivamente*/
 /*Muestra por consola la suma de ambas variables.*/
 /*Muestra por consola la resta de ambas variables.*/
