0a710324c164720777cc7050f29f475511088a80
mamugo1432 <mamugo1432@gmail.com>
2025-03-25 09:55:54 +0100

    Añadido ejercicio 2 realizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*Una variable numero cuyo valor sea -4.*/
 let numero=-4;
 /*Una variable palabra cuyo valor sea ”Hola Mundo”*/
+let palabra="Hola Mundo";
 /*Una variable booleana cuyo nombre sea respuesta e 
 inicialmente con valor true*/
 /*Crea una constante con nombre pi cuyo valor sea 3.14*/
