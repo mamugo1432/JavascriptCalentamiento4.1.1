27665bf41f6ceccffd1fb891cf52a5ae78365499
mamugo1432 <mamugo1432@gmail.com>
2025-03-25 09:58:06 +0100

    Añadido ejercicio 3 realizado

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let numero=-4;
 let palabra="Hola Mundo";
 /*Una variable booleana cuyo nombre sea respuesta e 
 inicialmente con valor true*/
+let respuesta=true; 
 /*Crea una constante con nombre pi cuyo valor sea 3.14*/
 /*Modifica el valor de la variable numero para que en lugar 
 de -4 sea 54 pero sin tocar la parte de la declaración de la variable.*/
